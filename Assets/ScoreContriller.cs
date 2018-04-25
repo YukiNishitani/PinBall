2984bfff252561397a9dfc36516e4e427eff5929
西谷祐樹 <the.heavy-works@outlook.jp>
2018-04-26 07:27:33 +0900

    2018/04/26 Lesson5課題提出
    ScorePrint.csからScoreContrill.csへのコンポーネント取得の方法がわかりませんでした。

---
+++ Assets/ScoreContriller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreContriller : MonoBehaviour{
+    int Score = 0;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "SmallStarTag")
+        {
+            Score += 100;
+        }
+        else if (other.transform.tag == "LatgeStarTag")
+        {
+            Score += 200;
+        }
+        else if (other.transform.tag == "SmallCloudTag")
+        {
+            Score += 50;
+        }
+        else if (other.transform.tag == "LargeCloudTag")
+        {
+            Score += 100;
+        }
+
+    }
+}
---
+++ Assets/ScorePrint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScorePrint : MonoBehaviour {
+    private GameObject scoreText;
+
+	void Start(){
+        this.scoreText = GameObject.Find("ScoreText");
+	}
+
+	// Update is called once per frame
+	void Update () {
+        this.scoreText = GameObject.Find("Ball");
+        this.scoreText.GetComponent<Text>().text = "Score:" + (Score);		
+	}
+}
